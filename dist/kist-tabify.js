076f398c3dc14780898edddb66e617232b17596f
Ivan Nikolić <ivan.nikolic@trikoder.net>
2015-03-02 10:45:14 +0100

    Release 0.2.3

--- dist/kist-tabify.js
+++ dist/kist-tabify.js
@@ -1,4 +1,4 @@
-/*! kist-tabify 0.2.2 - Simple tabs and accordion interface. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT */
+/*! kist-tabify 0.2.3 - Simple tabs and accordion interface. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.jQuery||(g.jQuery = {}));g=(g.fn||(g.fn = {}));g.tabify = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Tabify = require(3);
 var meta = require(10);
@@ -217,11 +217,6 @@ var Tabify = module.exports = Klass.extend({
 		instance.setup.call(this);
 		dom.setup.call(this);
 		events.setup.call(this);
-
-		if ( !this.$tab.is('a, button') ) {
-			this.destroy();
-			$.error('Tab items should only be anchors or button elements.');
-		}
 
 		this.move(this.options.initial);
 		this.setupDone = true;
